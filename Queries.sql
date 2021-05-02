f0f4165d9e547b5286f82e86f2ee8ac5fadcbf4a
badamson1 <72579889+badamson1@users.noreply.github.com>
2021-05-02 09:16:02 -0700

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -12,3 +12,27 @@ SELECT s.year,
 FROM senate_data AS s
 LEFT JOIN election_performace_indicators AS e 
 ON LOWER(e.state_fips) = LOWER(s.state)
+
+--Aggregating the senate_data table in a WITH subquery to get in the same table structure as election_performace_indicators
+WITH senate_agg AS (
+SELECT year,
+	   state,
+	   district,
+	   candidate,
+	   candidatevotes,
+	   totalvotes
+FROM senate_data
+	   )
+
+SELECT s.year, 
+	   state,  
+	   district, 
+	   candidate, 
+	   candidatevotes, 
+	   totalvotes,
+	   pct_reg_of_vep_vrs,
+	   vep_turnout
+FROM senate_data AS s
+LEFT JOIN election_performace_indicators AS e 
+ON LOWER(e.state_fips) = LOWER(s.state)
+GROUP BY s.year, state, district, candidate
